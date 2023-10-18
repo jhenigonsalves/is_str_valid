ed276cad4f49fc035b48aed183c4d96aa84c1d09
jheniffer <you@example.com>
2023-10-18 14:27:17 -0300

    creating test functions

---
+++ tests/test_isValid.py
@@ -0,0 +1,16 @@
+from main import isValid
+
+# Run these tests by invoking `$ python3 -m pytest tests`
+# https://docs.pytest.org/en/6.2.x/usage.html#:~:text=You%20can%20invoke%20testing%20through,the%20current%20directory%20to%20sys.
+
+
+def test_valid_str_in_isValid():
+    string_1 = "()"
+    string_2 = "()[]{}"
+    assert isValid(string_1)
+    assert isValid(string_2)
+
+
+def test_not_valid_str_in_isValid():
+    string_1 = "(]"
+    assert not isValid(string_1)
