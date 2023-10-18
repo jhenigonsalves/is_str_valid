db97c9743dc7fdaa30225ecb54b98b8a611557a8
jheniffer <you@example.com>
2023-10-18 14:37:39 -0300

    adding more test cases

--- tests/test_isValid.py
+++ tests/test_isValid.py
@@ -7,10 +7,19 @@ from main import isValid
 def test_valid_str_in_isValid():
     string_1 = "()"
     string_2 = "()[]{}"
+    string_3 = "([])"
     assert isValid(string_1)
     assert isValid(string_2)
+    assert isValid(string_3)
 
 
 def test_not_valid_str_in_isValid():
     string_1 = "(]"
+    string_2 = "([{})"
+    string_3 = "]()"
+    string_4 = "]()"
+
     assert not isValid(string_1)
+    assert not isValid(string_2)
+    assert not isValid(string_3)
+    assert not isValid(string_4)
